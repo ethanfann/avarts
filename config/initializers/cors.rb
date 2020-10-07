d39999ab072ace703bdf2aebca70707451f0d04a
ethanfann <ecfann@gmail.com>
2020-10-07 01:03:51 -0700

    Update routes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
 
     origins ENV['CLIENT_URL'] ? ENV['CLIENT_URL'].split(',').map { |origin| origin.strip } : '0.0.0.0:3001'
-		# origins ['localhost:3001', 'cryptic-brook-60963.herokuapp.com']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	devise_for :users
 
 	root 'application#frontend_index_html'
-	get '/*path', to: 'application#frontend_index_html', constraints: lambda { |request|
+	get '*', to: 'application#frontend_index_html', constraints: lambda { |request|
 		!request.xhr? && request.format.html?
 	}
 end
