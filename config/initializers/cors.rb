9fb582441db36d28ed99efb3ccfe9db7db44b12f
ethanfann <ecfann@gmail.com>
2020-10-06 22:15:35 -0700

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 		# origins ['localhost:3001', 'cryptic-brook-60963.herokuapp.com']
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
