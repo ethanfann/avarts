89b74e1962a0189f3f38f84d20e1de8bfd73ec7c
ethanfann <ecfann@gmail.com>
2020-10-07 00:32:37 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
 	devise_for :users
 
-	get '*path', to: 'application#frontend_index_html', constraints: lambda { |request|
+	root 'application#frontend_index_html'
+	get '/*path', to: 'application#frontend_index_html', constraints: lambda { |request|
 		!request.xhr? && request.format.html?
 	}
 end
