9d8b04ce999d627afd09835d35dd6bf659b0e3f0
ethanfann <ecfann@gmail.com>
2020-10-07 01:19:24 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	devise_for :users
 
 	root 'application#frontend_index_html'
-	get '*', to: 'application#frontend_index_html', constraints: lambda { |request|
+	get '*path', to: 'application#frontend_index_html', constraints: lambda { |request|
 		!request.xhr? && request.format.html?
 	}
 end
