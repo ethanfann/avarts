aef1bd1bcf5abff369ea14440ef73fe571914ef9
ethanfann <ecfann@gmail.com>
2020-10-11 15:45:38 -0700

    Temporarily don't load geojson

--- app/graphql/resolvers/activities_by_user_id.rb
+++ app/graphql/resolvers/activities_by_user_id.rb
@@ -2,10 +2,9 @@ class Resolvers::ActivitiesByUserId < GraphQL::Schema::Resolver
   argument :user_id, ID, required: true
 
   type [Types::ActivityType], null: true
-  description 'Returns the activities belonging to the specified user_id'
+  description "Returns the activities belonging to the specified user_id"
 
   def resolve(user_id: nil)
-    Activity.where(user_id: user_id).order("created_at DESC").includes(:user)
+    Activity.where(user_id: user_id).select(Activity.attribute_names - ["geoJson"]).order("created_at DESC").includes(:user)
   end
-
 end
