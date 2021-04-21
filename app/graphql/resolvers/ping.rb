002b26fbad780abd79629d245e708e02fde93835
Ethan Fann <ecfann@gmail.com>
2021-04-21 11:51:52 -0700

    Add ping query

---
+++ app/graphql/resolvers/ping.rb
@@ -0,0 +1,10 @@
+class Resolvers::Ping < GraphQL::Schema::Resolver
+  
+  type String, null: false
+  description 'Ping Pong'
+
+  def resolve
+    "Pong"
+  end
+
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,6 +2,7 @@ module Types
   class QueryType < BaseObject
     field :me, resolver: Resolvers::Me
     field :activities_by_user_id, resolver: Resolvers::ActivitiesByUserId
+    field :ping, resolver: Resolvers::Ping
   end
 end
   
