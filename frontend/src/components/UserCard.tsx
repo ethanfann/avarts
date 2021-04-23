25873db50266bf2daaa6ea42cc337d8c0b6fe4dc
Ethan Fann <git@ethanfann.com>
2021-04-23 14:35:03 -0700

    Improve latest activity usercard font size

--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -16,28 +16,19 @@ const UserCard: React.FC = () => {
                 className="img-fluid rounded-circle"
                 alt="rounded circle image"
               />
-              <h1
-                style={{ marginTop: 10 }}
-                className="card-title text-center font-weight-bold font-size-22 text-primary"
-              >
+              <h1 className="card-title text-center font-weight-bold font-size-22 text-primary mt-10 mb-10">
                 {ctx.user?.name}
               </h1>
-              <p className="font-weight-bold mb-0 pb-0">
+              <p className="font-weight-bold mb-0 pb-0 mt-0 font-size-14">
                 {ctx.user.activityCount} activities
               </p>
               <hr />
               {ctx.user.latestActivity.title !== '' && (
                 <>
-                  <p
-                    style={{ lineHeight: '17 .4px', fontSize: '10px' }}
-                    className="text-left mb-5 font-weight-semi-bold"
-                  >
+                  <p className="text-left mb-5 font-weight-semi-bold font-size-14">
                     Latest Activity
                   </p>
-                  <p
-                    style={{ fontSize: '10px' }}
-                    className=" text-left mt-0 pt-0"
-                  >
+                  <p className=" text-left mt-0 pt-0 font-size 12">
                     <strong>{ctx.user.latestActivity.title} • </strong>
                     <span className="font-weight-normal">
                       {DayJs(ctx.user.latestActivity.createdAt).format(
