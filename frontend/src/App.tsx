26f6f11271d3334d68892b4aa9a458c14223c860
Ethan Fann <git@ethanfann.com>
2021-04-23 16:00:39 -0700

    Add favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -104,6 +104,36 @@ const App: React.FC = () => {
                     <Helmet>
                       <meta charSet="utf-8" />
                       <title>Avarts</title>
+                      <meta
+                        name="description"
+                        content="Avarts is an open-source and self-hostable ride activity tracker."
+                      />
+                      <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                      />
+                      <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/apple-touch-icon.png"
+                      />
+                      <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon-32x32.png"
+                      />
+                      <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon-16x16.png"
+                      />
+                      <link
+                        rel="canonical"
+                        href="https://avarts.ethanfann.com"
+                      />
+                      <link rel="manifest" href="/site.webmanifest"></link>
                     </Helmet>
 
                     <BrowserRouter>
