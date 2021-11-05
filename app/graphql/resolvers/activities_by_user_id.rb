4f03a39e78cb9b6e8d1c883baf7a04e5331eb54e
Ethan Fann <github@ethanfann.com>
2021-11-04 17:16:01 -0700

    Restrict activites_by_user_id query

--- app/graphql/resolvers/activities_by_user_id.rb
+++ app/graphql/resolvers/activities_by_user_id.rb
@@ -2,9 +2,17 @@ class Resolvers::ActivitiesByUserId < GraphQL::Schema::Resolver
   argument :user_id, ID, required: true
 
   type [Types::ActivityType], null: true
-  description "Returns the activities belonging to the specified user_id"
+  description 'Returns the activities belonging to the specified user_id'
 
   def resolve(user_id: nil)
-    Activity.where(user_id: user_id).select(Activity.attribute_names - ["geoJson"]).order("created_at DESC").includes(:user)
+    if context[:current_user].nil?
+      raise GraphQL::ExecutionError, 'Not Authenticated'
+    end
+
+    if context[:current_user].id != user_id.to_i
+      raise GraphQL::ExecutionError, 'Access Denied'
+    end
+
+    Activity.where(user_id: user_id).order('created_at DESC').includes(:user)
   end
 end
