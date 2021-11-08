15ca58ff747a9b7be3baf444571e55f021e84dfd
Ethan Fann <github@ethanfann.com>
2021-11-07 19:06:51 -0800

    Use Terraform to manage S3

---
+++ terraform/variable.tf
@@ -0,0 +1,15 @@
+variable "aws_profile" {
+  default = "default"
+}
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "production_bucket_name" {
+  default = "avarts"
+}
+
+variable "dev_bucket_name" {
+  default = "avarts-dev"
+}
+
