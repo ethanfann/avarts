6c018f0e6627b835489ff5ab7d0baa7e219ebd92
Ethan Fann <github@ethanfann.com>
2022-02-12 20:04:27 -0800

    Replace the previous history entry

--- frontend/src/components/DetailedActivity/DetailedActivity.tsx
+++ frontend/src/components/DetailedActivity/DetailedActivity.tsx
@@ -59,7 +59,7 @@ const DetailedActivity = () => {
         },
       })
       if (result && result.data && result.data.deleteActivity) {
-        navigate('/')
+        navigate('/', { replace: true })
       }
     } catch (error) {
       console.log(error)
