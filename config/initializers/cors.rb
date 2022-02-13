fd6366919d89ed94cb358e1f461f5e8ca425fd10
Ethan Fann <github@ethanfann.com>
2022-02-13 12:07:38 -0800

    Set the Access-Control-Allow-Credentials header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,10 +12,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         ENV['CLIENT_URL'].split(',').map { |origin| origin.strip }
       else
         '0.0.0.0:3001'
-      end
+      end,
     )
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
