75d9f11716e41a815e1d39897868224d6aeeb196
Ethan Fann <github@ethanfann.com>
2022-02-12 21:09:54 -0800

    Refactor metrics

--- frontend/src/components/DetailedActivity/DetailedActivity.tsx
+++ frontend/src/components/DetailedActivity/DetailedActivity.tsx
@@ -57,7 +57,7 @@ const DetailedActivity = () => {
         variables: {
           activityId: id,
         },
-        refetchQueries: ['me'],
+        refetchQueries: ['me', 'myActivities'],
       })
       if (result && result.data && result.data.deleteActivity) {
         navigate('/', { replace: true })
@@ -65,6 +65,53 @@ const DetailedActivity = () => {
     } catch (error) {
       console.log(error)
     }
+  }
+
+  const Table = (
+    avgSpeed: number,
+    maxSpeed: number,
+    avgPower: number,
+    maxPower: number,
+    avgHr: number,
+    maxHr: number,
+    avgCadence: number,
+    maxCadence: number
+  ) => {
+    return (
+      <table className="table mt-20">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Avg</th>
+            <th>Max</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Speed ({user.measurementPreference === 'feet' ? 'mph' : 'kph'})
+            </td>
+            <td>{formatSpeed(avgSpeed, user.measurementPreference)}</td>
+            <td>{formatSpeed(maxSpeed, user.measurementPreference)}</td>
+          </tr>
+          <tr>
+            <td>Power (w)</td>
+            <td>{avgPower}</td>
+            <td>{maxPower}</td>
+          </tr>
+          <tr>
+            <td>Heart Rate</td>
+            <td>{avgHr}</td>
+            <td>{maxHr}</td>
+          </tr>
+          <tr>
+            <td>Cadence</td>
+            <td>{avgCadence}</td>
+            <td>{maxCadence}</td>
+          </tr>
+        </tbody>
+      </table>
+    )
   }
 
   if (loading) {
@@ -77,51 +124,102 @@ const DetailedActivity = () => {
 
   if (error) return <div>Error</div>
 
-  if (isMobile) {
-    return (
-      <div>
-        {data && data.activityById && (
-          <div className="w-full w-lg mt-10">
-            <div className="card border-0">
-              <div className="px-card border-bottom">
-                <p style={{ fontSize: 34 }} className="m-0 font-weight-bold">
-                  {data.activityById.user.name} - Ride
-                </p>
-              </div>
-              <div className="content">
-                <div className="container-fluid">
-                  <div className="row mb-20">
-                    <div className="col-2">
-                      <div className="d-flex flex-column mr-10">
-                        <div className="text-center">
-                          <img
-                            className="img-fluid rounded-circle"
-                            src={
-                              data.activityById.user.img
-                                ? data.activityById.user.img
-                                : 'default-user-avatar.png'
-                            }
-                            width={100}
-                            height={100}
-                            alt="user avatar"
-                          ></img>
-                        </div>
-                      </div>
+  return (
+    <div>
+      {data && data.activityById && (
+        <div className="w-full mt-10">
+          <div className="card border-0 mt-0">
+            <div className="px-card border-bottom d-flex align-items-center">
+              <p
+                style={{ fontSize: 28 }}
+                className="m-0 font-weight-bold w-full"
+              >
+                {data.activityById.user.name} - Ride
+              </p>
+              <div className="flex-shrink-1">
+                <div className="dropdown dropleft with-arrow">
+                  <button
+                    className="btn"
+                    data-toggle="dropdown"
+                    type="button"
+                    aria-label="view more activity options"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    <FontAwesomeIcon icon={faEllipsisV} />
+                  </button>
+                  <div className="dropdown-menu dropdown-menu-center">
+                    <div className="dropdown-content">
+                      <button
+                        className="btn btn-block"
+                        onClick={(e) => handleDelete(e, data.activityById.id)}
+                        aria-label={'delete activity'}
+                      >
+                        <FontAwesomeIcon icon={faTrash} />
+                      </button>
                     </div>
-                    <div className="col-10">
-                      <div className="m-5">
-                        {activityTime(data.activityById.startTime)}
-                        <p className="font-weight-bold m-0 font-size-24">
-                          {data.activityById.title}
-                        </p>
-                        {data.activityById.description !== '' && (
-                          <p className="mb-10 font-size-12">
-                            {data.activityById.description}
-                          </p>
-                        )}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="content">
+              <div className="container-fluid">
+                <div className="row mb-20">
+                  <div className="col-2">
+                    <div className="d-flex flex-column mr-10">
+                      <div className="text-center">
+                        <img
+                          className="img-fluid rounded-circle"
+                          src={
+                            data.activityById.user.img
+                              ? data.activityById.user.img
+                              : 'default-user-avatar.png'
+                          }
+                          width={100}
+                          height={100}
+                          alt="user avatar"
+                        ></img>
                       </div>
                     </div>
                   </div>
+                  <div className={`col-${isMobile ? 10 : 4}`}>
+                    <div className="m-5">
+                      {activityTime(data.activityById.startTime)}
+                      <p className="font-weight-bold m-0 font-size-24">
+                        {data.activityById.title}
+                      </p>
+                      {data.activityById.description !== '' && (
+                        <p className="mb-10 font-size-12">
+                          {data.activityById.description}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  {!isMobile && (
+                    <div className="col-6">
+                      <div className="d-flex justify-content-center">
+                        <MetricRow
+                          duration={data.activityById.duration}
+                          elevation={data.activityById.elevation}
+                          distance={data.activityById.distance}
+                        />
+                      </div>
+                      <div className="table-responsive">
+                        {Table(
+                          data.activityById.avgSpeed,
+                          data.activityById.maxSpeed,
+                          data.activityById.avgPower,
+                          data.activityById.maxPower,
+                          data.activityById.avgHr,
+                          data.activityById.maxHr,
+                          data.activityById.avgCadence,
+                          data.activityById.maxCadence
+                        )}
+                      </div>
+                    </div>
+                  )}
+                </div>
+                {isMobile && (
                   <div className="row mb-20">
                     <div className="col-12">
                       <div className="d-flex justify-content-center">
@@ -132,224 +230,37 @@ const DetailedActivity = () => {
                         />
                       </div>
                       <div className="table-responsive">
-                        <table className="table mt-20">
-                          <thead>
-                            <tr>
-                              <th></th>
-                              <th>Avg</th>
-                              <th>Max</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>
-                                Speed (
-                                {user.measurementPreference === 'feet'
-                                  ? 'mph'
-                                  : 'kph'}
-                                )
-                              </td>
-                              <td>
-                                {formatSpeed(
-                                  data.activityById.avgSpeed,
-                                  user.measurementPreference
-                                )}
-                              </td>
-                              <td>
-                                {formatSpeed(
-                                  data.activityById.maxSpeed,
-                                  user.measurementPreference
-                                )}
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>Power (w)</td>
-                              <td>{data.activityById.avgPower}</td>
-                              <td>{data.activityById.maxPower}</td>
-                            </tr>
-                            <tr>
-                              <td>Heart Rate</td>
-                              <td>{data.activityById.avgHr}</td>
-                              <td>{data.activityById.maxHr}</td>
-                            </tr>
-                            <tr>
-                              <td>Cadence</td>
-                              <td>{data.activityById.avgCadence}</td>
-                              <td>{data.activityById.maxCadence}</td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                  <div
-                    style={{ height: '100%', width: '100%', marginBottom: 10 }}
-                  >
-                    {data && data.activityById && (
-                      <MapBoxView
-                        initPolyline={data.activityById.polyline}
-                        mapboxToken={user.mapboxToken}
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        {data && data.activityById && (
-          <div className="w-full mt-10">
-            <div className="card border-0 mt-0">
-              <div className="px-card border-bottom d-flex">
-                <p
-                  style={{ fontSize: 34 }}
-                  className="m-0 font-weight-bold w-full"
-                >
-                  {data.activityById.user.name} - Ride
-                </p>
-                <div className="flex-shrink-1">
-                  <div className="dropdown dropdown with-arrow">
-                    <button
-                      className="btn"
-                      data-toggle="dropdown"
-                      type="button"
-                      id="..."
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <FontAwesomeIcon icon={faEllipsisV} />
-                    </button>
-                    <div
-                      className="dropdown-menu dropdown-menu-center"
-                      aria-labelledby="..."
-                    >
-                      <div className="dropdown-content">
-                        <button
-                          className="btn btn-block"
-                          onClick={(e) => handleDelete(e, data.activityById.id)}
-                        >
-                          <FontAwesomeIcon icon={faTrash} />
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="content">
-                <div className="container-fluid">
-                  <div className="row mb-20">
-                    <div className="col-2">
-                      <div className="d-flex flex-column mr-10">
-                        <div className="text-center">
-                          <img
-                            className="img-fluid rounded-circle"
-                            src={
-                              data.activityById.user.img
-                                ? data.activityById.user.img
-                                : 'default-user-avatar.png'
-                            }
-                            width={100}
-                            height={100}
-                            alt="user avatar"
-                          ></img>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-4">
-                      <div className="m-5">
-                        {activityTime(data.activityById.startTime)}
-                        <p className="font-weight-bold m-0 font-size-24">
-                          {data.activityById.title}
-                        </p>
-                        {data.activityById.description !== '' && (
-                          <p className="mb-10 font-size-12">
-                            {data.activityById.description}
-                          </p>
+                        {Table(
+                          data.activityById.avgSpeed,
+                          data.activityById.maxSpeed,
+                          data.activityById.avgPower,
+                          data.activityById.maxPower,
+                          data.activityById.avgHr,
+                          data.activityById.maxHr,
+                          data.activityById.avgCadence,
+                          data.activityById.maxCadence
                         )}
                       </div>
                     </div>
-                    <div className="col-6">
-                      <div className="d-flex justify-content-center">
-                        <MetricRow
-                          duration={data.activityById.duration}
-                          elevation={data.activityById.elevation}
-                          distance={data.activityById.distance}
-                        />
-                      </div>
-                      <div className="table-responsive">
-                        <table className="table mt-20">
-                          <thead>
-                            <tr>
-                              <th></th>
-                              <th>Avg</th>
-                              <th>Max</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>
-                                Speed (
-                                {user.measurementPreference === 'feet'
-                                  ? 'mph'
-                                  : 'kph'}
-                                )
-                              </td>
-                              <td>
-                                {formatSpeed(
-                                  data.activityById.avgSpeed,
-                                  user.measurementPreference
-                                )}
-                              </td>
-                              <td>
-                                {formatSpeed(
-                                  data.activityById.maxSpeed,
-                                  user.measurementPreference
-                                )}
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>Power (w)</td>
-                              <td>{data.activityById.avgPower}</td>
-                              <td>{data.activityById.maxPower}</td>
-                            </tr>
-                            <tr>
-                              <td>Heart Rate</td>
-                              <td>{data.activityById.avgHr}</td>
-                              <td>{data.activityById.maxHr}</td>
-                            </tr>
-                            <tr>
-                              <td>Cadence</td>
-                              <td>{data.activityById.avgCadence}</td>
-                              <td>{data.activityById.maxCadence}</td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
                   </div>
-                  <div
-                    style={{ height: '100%', width: '100%', marginBottom: 10 }}
-                  >
-                    {data && data.activityById && (
-                      <MapBoxView
-                        initPolyline={data.activityById.polyline}
-                        mapboxToken={user.mapboxToken}
-                      />
-                    )}
-                  </div>
+                )}
+                <div
+                  style={{ height: '100%', width: '100%', marginBottom: 10 }}
+                >
+                  {data && data.activityById && (
+                    <MapBoxView
+                      initPolyline={data.activityById.polyline}
+                      mapboxToken={user.mapboxToken}
+                    />
+                  )}
                 </div>
               </div>
             </div>
           </div>
-        )}
-      </div>
-    )
-  }
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default DetailedActivity
