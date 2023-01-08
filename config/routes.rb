4828ad2045b5ced2f7e74b2a5b644677fe902de6
Ethan Fann <6005240+ethanfann@users.noreply.github.com>
2023-01-08 14:10:29 -0800

    Remove frontend route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
   devise_for :users
-
-  root 'static#frontend_index_html'
-  get '*path',
-      to: 'static#frontend_index_html',
-      constraints: lambda { |request| !request.xhr? && request.format.html? }
 end
