51786ddec78b156c89a48b61161dc89d70ae3fb1
Ethan Fann <6005240+ethanfann@users.noreply.github.com>
2023-01-08 14:10:00 -0800

    Remove rails features

--- app/controllers/static_controller.rb
+++
@@ -1,5 +0,0 @@
-class StaticController < ActionController::Base
-  def frontend_index_html
-    render file: 'public/index.html'
-  end
-end
--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ require 'active_storage/engine'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
-require 'action_cable/engine'
-# require "sprockets/railtie"
-require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,7 +18,7 @@ module RailsDeviseGraphql
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     # config.load_defaults 5.2
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     config.autoload_paths << "#{Rails.root}/lib"
 
