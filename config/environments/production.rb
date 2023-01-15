9bec2329dc4dc310b3089569911e594be1a79757
Ethan Fann <git@ethanfann.com>
2023-01-15 15:50:04 -0800

    Enable activerecord logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.active_record.logger = nil
-
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
