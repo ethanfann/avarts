b7a6173dc5095648a093e13c68cb32a0480e57ba
Ethan Fann <git@ethanfann.com>
2023-01-15 14:22:44 -0800

    Add db commands

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -6,4 +6,7 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
 bundle exec rails s -b 0.0.0.0
